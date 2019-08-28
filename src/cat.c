bcc0f948969cf5e95f2627a8cbe8a059f877d6c7
mckenney5 <mckenneyadam@gmail.com>
2019-08-28 00:16:19 -0400

    Cleaned up code, added comments

--- src/cat.c
+++ src/cat.c
@@ -1,10 +1,11 @@
 /* Copyright 2019 (c) Adam McKenney - See LICENSE for details */
 /* A way slimmer version of cat without the GNU bloat features that I have never used nor needed */
 #include <stdio.h>   //fgetc, putchar, FILE, fopen, perror, fclose, puts, fprintf
-#define NAME "cat"
-#define HELP "Usage: " NAME " [-hu] [file...]\nWe concatenate files(s) to standard output with less than 60 lines of code.\n\nWith no file, or when file is -, read standard input.\nExamples:\n  " NAME " -h     Displays the help\n  " NAME " -u     Tells the OS that we dont want a buffer (warning may be a lot slower with a large file).\n  " NAME " x - y  Output x's contents, then standard input, then y's contents.\n  " NAME "        Copy standard input to standard output."
+#define NAME "cat"   //Program name, can be changed so a used can keep their bloated cat
+#define HELP "Usage: " NAME " [-hu] [file...]\nWe concatenate files(s) to standard output with less than 50 lines of code.\n\nWith no file, or when file is -, read standard input.\nExamples:\n  " NAME " -h     Displays the help\n  " NAME " -u     Tells the OS that we dont want a buffer (warning may be a lot slower with a large file).\n  " NAME " x - y  Output x's contents, then standard input, then y's contents.\n  " NAME "        Copy standard input to standard output."
 static short no_buf = 0; //-u option
 void throw_error(char* extra_msg){
+/* function to write errors to stderr */
 	if(extra_msg != NULL) fprintf(stderr, "%s: %s: ", NAME, extra_msg);
 	else fprintf(stderr, "%s: ", NAME);
 	perror("");
@@ -16,10 +17,7 @@ void cat(FILE *fp){
 	if(no_buf) if(setvbuf(fp, &p, _IONBF, 1)) throw_error(NULL); //tell the OS we dont want a buffer
 	do {
 		status = fread(&p, 1, 1, fp);
-		if(status != 0){ 
-			fwrite(&p, 1, 1, stdout);
-			//if(no_buf) fflush(fp); //-u option
-		}
+		if(status != 0) fwrite(&p, 1, 1, stdout);
 	} while(status != 0);
 }
 int get_file(char* file_name){
