70a770d82ba42599ea9e252516d4741e30101a3d
mckenney5 <mckenneyadam@gmail.com>
2019-08-29 09:11:04 -0400

    Updated comments

--- src/cat.c
+++ src/cat.c
@@ -1,11 +1,11 @@
 /* Copyright 2019 (c) Adam McKenney - See LICENSE for details */
 /* A way slimmer version of cat without the GNU bloat features that I have never used nor needed */
 #include <stdio.h>   //fgetc, putchar, FILE, fopen, perror, fclose, puts, fprintf
-#define NAME "cat"   //Program name, can be changed so a used can keep their bloated cat
+#define NAME "cat"   //Program name, can be changed so a user can keep their bloated cat
 #define HELP "Usage: " NAME " [-hu] [file...]\nWe concatenate files(s) to standard output with less than 50 lines of code.\n\nWith no file, or when file is -, read standard input.\nExamples:\n  " NAME " -h     Displays the help\n  " NAME " -u     Tells the OS that we dont want a buffer (warning may be a lot slower with a large file).\n  " NAME " x - y  Output x's contents, then standard input, then y's contents.\n  " NAME "        Copy standard input to standard output."
 static short no_buf = 0; //-u option
 void throw_error(char* extra_msg){
-/* function to write errors to stderr */
+/* function to writes the program name, optionally extra info, then the error to stderr */
 	if(extra_msg != NULL) fprintf(stderr, "%s: %s: ", NAME, extra_msg);
 	else fprintf(stderr, "%s: ", NAME);
 	perror("");
@@ -21,9 +21,9 @@ void cat(FILE *fp){
 	} while(status != 0);
 }
 int get_file(char* file_name){
-/* tries to open the user file, then calls cat() with its file pointer */
+/* tries to open the user's file, then calls cat() with its file pointer */
 	FILE *fp = NULL;
-	if(file_name[0] == '-' && file_name[1] == '\0') fp = stdin;
+	if(file_name[0] == '-' && file_name[1] == '\0') fp = stdin; //if the user did 'cat -' or 'cat foo - bar'
 	else fp = fopen(file_name, "r");
 
 	if(fp == NULL){
@@ -39,12 +39,12 @@ int main(int argc, char *argv[]){
 	for(i=1; i < argc; i++){
 		if(argv[i][0] == '-' && argv[i][1] != '\0'){ //if its a command line arg
 			if(argv[i][1] == 'h') { puts(HELP); return 0; }
-			else if(argv[i][1] == 'u') no_buf = 1;
+			else if(argv[i][1] == 'u') no_buf = 1; //required by POSIX, turns off buffered input
 			else { fprintf(stderr, "%s: unknown command line arg '%s'\n", NAME, argv[i]); return 2;}
 		} else break;
 	}
-	if(argv[i] == NULL) cat(stdin);
-	else for(i=i; i < argc; i++) get_file(argv[i]); //step through each arg as if its a file
+	if(argv[i] == NULL) cat(stdin); //if no args
+	else for(i=i; i < argc; i++) get_file(argv[i]); //step through each arg as if it's a file
 	return 0;
 }
 
